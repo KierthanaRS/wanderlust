a07c9421cd36e718964c0e111e6af54e7bafd623
Kierthana <77330790+KierthanaRS@users.noreply.github.com>
2023-05-29 12:41:04 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,7 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+function toggleMenu() {
+      const menuContainer = document.querySelector('.menu-container');
+      menuContainer.classList.toggle('show-menu');
+    }
